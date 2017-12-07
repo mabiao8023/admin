8e955f86a94711166de8d1a5cd870df6115ea8e4
mabiao <mabiao@linghit.com>
2017-12-07 22:20:29 +0800

    去掉查询

--- src/views/nav1/user.vue
+++ src/views/nav1/user.vue
@@ -1,7 +1,7 @@
 <template>
 	<section>
 		<!--工具条-->
-		<el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
+		<!-- <el-col :span="24" class="toolbar" style="padding-bottom: 0px;">
 			<el-form :inline="true" :model="filters">
 				<el-form-item>
 					<el-input v-model="filters.name" placeholder="姓名"></el-input>
@@ -10,7 +10,7 @@
 					<el-button type="primary" v-on:click="getUser">查询</el-button>
 				</el-form-item>
 			</el-form>
-		</el-col>
+		</el-col> -->
 
 		<!--列表-->
 		<template>
